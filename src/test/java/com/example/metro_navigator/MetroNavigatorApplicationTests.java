38d4549770d7defdda6594022b8f80e7d9d251f6
dev-shreyash <bhosaleshreyash2@gmail.com>
2026-02-27 18:48:09 +0530

    project init

---
+++ src/test/java/com/example/metro_navigator/MetroNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metro_navigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
